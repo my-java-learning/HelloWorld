51086938fd9af841acf4b76f91cf65724358be1d
Qsior <lukaszbojes@gmail.com>
2017-10-07 22:55:00 +0200

    Data types, second lesson

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
+        int myFirstNumer = 10 + 5 + 2 * 10;
+        int mySecondNumer = 12;
+        int myThirdNumer = myFirstNumer * 2;
+
+        int myTotal = myFirstNumer + mySecondNumer + myThirdNumer;
+        int myDifference = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myDifference);
     }
 }
