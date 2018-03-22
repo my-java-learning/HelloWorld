cb6df4df53912b565c428019e854d480e1ba9ba0
lukasbs <lukaszbojes@gmail.com>
2018-03-22 21:52:51 +0100

    Another test

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
         int myFirstNumer = 10 + 5 + 2 * 10;
         int mySecondNumer = 12;
         int myThirdNumer = myFirstNumer * 2;
-
         int myTotal = myFirstNumer + mySecondNumer + myThirdNumer;
         int myDifference = 1000 - myTotal;
 
